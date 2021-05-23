bada2ffacf3b5b79cba9ef0cdbc18c2779c117df
Hungng <hungnguyen35800@gmail.com>
2021-05-23 12:08:35 +0700

    done profile

--- php/editprofile.php
+++ php/editprofile.php
@@ -82,9 +82,9 @@ $pn = htmlspecialchars($student['PhoneNum'])
     <div class="fixed">
     <div class="brandname"><?=htmlspecialchars($student_name) ?>'s<br> Utilities</div>
     <div class="menu">
-    <div class="navbox1"><a href="profile.php" class="none">Profile</a></div>
+    <div class="navbox2"><a href="profile.php" class="none">Profile</a></div>
     <div class="navbox1">Academic Transcript</div>
-    <div class="navbox2"><a href="timetable.php" class="none">Time Table</a></div>
+    <div class="navbox1"><a href="timetable.php" class="none">Time Table</a></div>
     <div class="navbox1"><a href="teachercontact.php" class="none">Teachers' Contacts</a></div>
     <div class="navbox1"><a href="logout.php" class="none">Log out</a></div>
     </div>
